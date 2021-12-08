3e92bd546421928497783993325c8c72dd48b7c7
silviney <silvineyf@gmail.com>
2021-12-08 10:53:23 -0300

    1º commit - criação dos tipos enumerados

---
+++ src/test/java/com/silviney/sistemacadastro/SistemaCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silviney.sistemacadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
