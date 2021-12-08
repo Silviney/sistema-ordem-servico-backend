82f900f874e7b3323154564f72cd69d3f60c451d
silviney <silvineyf@gmail.com>
2021-12-08 11:07:29 -0300

    5º commit - Padrão DTO para tecnico, criação do findById, findAll e exceções personalizadas para ID de Tecnico não encontrado.

---
+++ src/main/java/com/silviney/sistemacadastro/domain/resources/TecnicoResource.java
@@ -0,0 +1,38 @@
+package com.silviney.sistemacadastro.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.silviney.sistemacadastro.domain.Tecnico;
+import com.silviney.sistemacadastro.domain.dtos.TecnicoDTO;
+import com.silviney.sistemacadastro.services.TecnicoService;
+	
+	@RestController
+	@RequestMapping(value ="/tecnicos")
+	public class TecnicoResource {
+		
+		@Autowired
+		private TecnicoService tecnicoService; 
+		
+		@GetMapping(value = "/{id}")
+		public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
+			 Tecnico obj = tecnicoService.findById(id);
+			 return ResponseEntity.ok().body(new TecnicoDTO(obj));
+		}
+		
+		@GetMapping
+		public ResponseEntity<List<TecnicoDTO>> findAll(){
+			List<Tecnico> list = tecnicoService.findAll();
+			List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDTO);
+		}
+		
+	}
+
